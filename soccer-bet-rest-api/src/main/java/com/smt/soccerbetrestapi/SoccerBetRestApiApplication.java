1eadf3a7c17d4e24364ab57f8f7de1cf5d267d48
msun <msun@nla.gov.au>
2020-12-17 17:51:30 +1100

    add webmvc unittest

--- soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/SoccerBetRestApiApplication.java
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/SoccerBetRestApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.soccerbetrestapi;
+package com.smt.soccerbetrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ soccer-bet-rest-api/src/test/java/com/smt/soccerbetrestapi/controller/HomeControllerTest.java
@@ -0,0 +1,25 @@
+package com.smt.soccerbetrestapi.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome")));
+    }
+}
