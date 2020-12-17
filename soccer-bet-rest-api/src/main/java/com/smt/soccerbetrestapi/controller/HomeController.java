6fa39e50afe211896b8e28a5ec7c672bef6d31f8
msun <msun@nla.gov.au>
2020-12-17 17:51:30 +1100

    add webmvc unittest

---
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.smt.soccerbetrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome";
+    }
+}
