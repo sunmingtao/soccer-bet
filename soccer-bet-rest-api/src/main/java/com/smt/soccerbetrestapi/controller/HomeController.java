fcc0d1b16fba421961186b222667b767bbc0664e
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2020-12-24 08:39:03 +1100

    integrate with dynamodb

--- soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/controller/HomeController.java
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.smt.soccerbetrestapi.controller;
 
 import com.smt.soccerbetrestapi.enums.League;
+import com.smt.soccerbetrestapi.model.Match;
 import com.smt.soccerbetrestapi.model.MatchStats;
+import com.smt.soccerbetrestapi.model.SimpleMatch;
 import com.smt.soccerbetrestapi.model.Team;
+import com.smt.soccerbetrestapi.repo.MatchRepo;
+import com.smt.soccerbetrestapi.repo.SimpleMatchRepo;
 import com.smt.soccerbetrestapi.repo.TeamRepo;
-import com.smt.soccerbetrestapi.utils.HtmlParser;
+import com.smt.soccerbetrestapi.service.SoccerDataService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,19 +19,27 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final SoccerDataService soccerDataService;
+    private final MatchRepo matchRepo;
+    private final SimpleMatchRepo simpleMatchRepo;
 
     @PostConstruct
     public void init() {
-        HtmlParser.loadLeague(League.LALIGA);
-        HtmlParser.loadLeague(League.SERIE_A);
-        HtmlParser.loadLeague(League.EPL);
-        HtmlParser.loadLeague(League.LIGUE_1);
-        HtmlParser.loadLeague(League.BUNDESLIGA);
+        soccerDataService.loadLeague(League.LALIGA);
+        soccerDataService.loadLeague(League.SERIE_A);
+        soccerDataService.loadLeague(League.EPL);
+        soccerDataService.loadLeague(League.LIGUE_1);
+        soccerDataService.loadLeague(League.BUNDESLIGA);
     }
 
     @GetMapping("/")
     public String home() {
+        SimpleMatch match = new SimpleMatch();
+        match.setMatchDate("2020");
+        simpleMatchRepo.save(match);
         return "Welcome";
     }
 
---
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/repo/MatchRepo.java
@@ -0,0 +1,10 @@
+package com.smt.soccerbetrestapi.repo;
+
+import com.smt.soccerbetrestapi.model.Match;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface MatchRepo extends CrudRepository<Match, String> {
+
+}
