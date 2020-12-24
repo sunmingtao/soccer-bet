cea5acebdad1d96b45d8763ba6ebf5684be55368
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2020-12-25 07:22:06 +1100

    add season utils

---
+++ soccer-bet-rest-api/src/test/java/com/smt/soccerbetrestapi/entity/MatchTest.java
@@ -0,0 +1,25 @@
+package com.smt.soccerbetrestapi.entity;
+
+import com.smt.soccerbetrestapi.model.Team;
+import com.smt.soccerbetrestapi.utils.SeasonUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+class MatchTest {
+    @Test
+    void equals() {
+        Match match1 = new Match(SeasonUtils.fromFullDateString("2020/1/2"), new Team("liverpool"), new Team("man city"), 1, 0, 0.25d, 0.25d);
+        Match match2 = new Match(SeasonUtils.fromFullDateString("2020/1/2"), new Team("liverpool"), new Team("man city"), 1, 1, 0.25d, 0.25d);
+        Assertions.assertTrue(match1.equals(match2));
+    }
+
+    @Test
+    void notEquals() {
+        Match match1 = new Match(SeasonUtils.fromFullDateString("2020/1/2"), new Team("liverpool"), new Team("man city"), 1, 0, 0.25d, 0.25d);
+        Match match2 = new Match(SeasonUtils.fromFullDateString("2020/1/3"), new Team("liverpool"), new Team("man city"), 1, 0, 0.25d, 0.25d);
+        Assertions.assertFalse(match1.equals(match2));
+    }
+}
