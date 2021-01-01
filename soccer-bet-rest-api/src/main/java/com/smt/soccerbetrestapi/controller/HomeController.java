d952a8dccacc4ff4246ab14613dddb9ca2c89022
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-01-01 18:35:42 +1100

    add more leagues and more stats

--- soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/controller/HomeController.java
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.smt.soccerbetrestapi.cron.MatchLoader;
 import com.smt.soccerbetrestapi.enums.League;
 import com.smt.soccerbetrestapi.model.MatchStats;
 import com.smt.soccerbetrestapi.model.Team;
+import com.smt.soccerbetrestapi.model.TeamList;
 import com.smt.soccerbetrestapi.repo.MatchRepo;
 import com.smt.soccerbetrestapi.service.MatchStatsService;
 import lombok.RequiredArgsConstructor;
@@ -25,20 +26,6 @@ public class HomeController {
     @PostConstruct
     public void init() {
         matchLoader.load();
-        printAllProfit();
-    }
-
-    public void printAllProfit() {
-        System.out.println("Number of matches: "
-                + matchStatsService.findTeams().stream().flatMap(team -> team.getMatchStatsList().stream()).count() / 2);
-        System.out.println("Total profit: " + matchStatsService.findTeams().stream().map(Team::getTotalProfit).reduce(0d, Double::sum));
-        System.out.println("Total profit as fav: " + matchStatsService.findTeams().stream().map(Team::getTotalProfitAsFav).reduce(0d, Double::sum));
-        System.out.println("Total profit back draw: " + matchStatsService.findTeams().stream().flatMap(team -> team.getMatchStatsList().stream())
-                .map(MatchStats::getProfitBackOnDraw).reduce(0d, Double::sum) / 2);
-        System.out.println("Total profit lay draw (Fixed win): " + matchStatsService.findTeams().stream().flatMap(team -> team.getMatchStatsList().stream())
-                .map(MatchStats::getProfitLayOnDraw).reduce(0d, Double::sum) / 2);
-        System.out.println("Total profit lay draw2 (Fixed liability): " + matchStatsService.findTeams().stream().flatMap(team -> team.getMatchStatsList().stream())
-                .map(MatchStats::getProfitLayOnDraw2).reduce(0d, Double::sum) / 2);
     }
 
     @GetMapping("/")
@@ -51,14 +38,9 @@ public class HomeController {
         return List.of(League.values());
     }
 
-    @GetMapping("/teams")
-    public List<Team> teams() {
-        return matchStatsService.findTeams();
-    }
-
     @GetMapping("/teams/{league}")
-    public List<Team> teamsByLeague(@PathVariable String league) {
-        return matchStatsService.findTeams(league);
+    public TeamList teamsByLeague(@PathVariable String league) {
+        return new TeamList(matchStatsService.findTeams(league));
     }
 
     @GetMapping("/team")
---
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/model/TeamList.java
@@ -0,0 +1,51 @@
+package com.smt.soccerbetrestapi.model;
+
+import com.smt.soccerbetrestapi.utils.DoubleUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class TeamList {
+    private final List<Team> teams;
+
+    public double getTotalProfitForLay() {
+        double result = teams.stream().map(Team::getTotalProfit).reduce(0d, Double::sum);
+        return DoubleUtils.round(result, 2);
+    }
+
+    public double getTotalProfitForLayAsFav() {
+        double result = teams.stream().map(Team::getTotalProfitAsFav).reduce(0d, Double::sum);
+        return DoubleUtils.round(result, 2);
+    }
+
+    public double getTotalProfitForLayAsStrictFav() {
+        double result = teams.stream().map(Team::getTotalProfitAsStrictFav).reduce(0d, Double::sum);
+        return DoubleUtils.round(result, 2);
+    }
+
+    public double getTotalProfitForBackDraw() {
+        double result = teams.stream().map(Team::getTotalProfitBackOnDraw).reduce(0d, Double::sum) / 2;
+        return DoubleUtils.round(result, 2);
+    }
+
+    public double getTotalProfitForLayDrawFixedWin() {
+        double result = teams.stream().flatMap(team -> team.getMatchStatsList().stream()).map(MatchStats::getProfitLayOnDrawFixedWin).reduce(0d, Double::sum);
+        return DoubleUtils.round(result, 2);
+    }
+
+    public double getTotalProfitForLayDrawFixedLiability() {
+        double result = teams.stream().flatMap(team -> team.getMatchStatsList().stream()).map(MatchStats::getProfitLayOnDrawFixedLiability).reduce(0d, Double::sum);
+        return DoubleUtils.round(result, 2);
+    }
+
+    public int getTotalDrawCount() {
+        return (int)teams.stream().flatMap(team -> team.getMatchStatsList().stream()).filter(matchStats -> matchStats.getActualPoints() == 1).count() / 2;
+    }
+
+    public int getTotalMatchCount() {
+        return (int)teams.stream().flatMap(team -> team.getMatchStatsList().stream()).count() / 2;
+    }
+}
