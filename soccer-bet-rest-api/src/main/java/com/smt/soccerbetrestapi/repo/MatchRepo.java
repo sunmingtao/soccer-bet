bca07abc4ffdc901e6e621303952dbaef8e38853
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-01-11 19:20:09 +1100

    find matches by season - backend

--- soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/repo/MatchRepo.java
+++ soccer-bet-rest-api/src/main/java/com/smt/soccerbetrestapi/repo/MatchRepo.java
@@ -4,7 +4,9 @@ import com.smt.soccerbetrestapi.entity.Match;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableScan
 public interface MatchRepo extends CrudRepository<Match, String> {
-
+    List<Match> findBySeason(String season);
 }
