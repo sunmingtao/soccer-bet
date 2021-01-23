5fddde547e69722f204a2f7e05bd70e44d9a04f5
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-01-24 07:55:42 +1100

    add nba project skeleton

---
+++ nba/src/main/java/com/smt/nba/NbaApplication.java
@@ -0,0 +1,13 @@
+package com.smt.nba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaApplication.class, args);
+	}
+
+}
