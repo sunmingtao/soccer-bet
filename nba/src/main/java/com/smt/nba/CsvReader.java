bece48e0c0168c7783c55515694a89808def4ed8
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-01-25 07:12:13 +1100

    add nba csv data and opencsv dependency

---
+++ nba/src/main/java/com/smt/nba/CsvReader.java
@@ -0,0 +1,27 @@
+package com.smt.nba;
+
+import com.opencsv.CSVReader;
+
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+
+    public static void main(String[] args) throws Exception {
+        Reader reader = Files.newBufferedReader(Paths.get(
+                ClassLoader.getSystemResource("nba_betting_money_line.csv").toURI()));
+        System.out.println(readAll(reader).size());
+    }
+
+    public static List<String[]> readAll(Reader reader) throws Exception {
+        CSVReader csvReader = new CSVReader(reader);
+        List<String[]> list = new ArrayList<>();
+        list = csvReader.readAll();
+        reader.close();
+        csvReader.close();
+        return list;
+    }
+}
