42d63a1b035b3c1f94bbc388330ff30cc7ea38e3
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-02-07 06:20:29 +1100

    add jackson library

--- betfair/src/main/java/com/smt/betfair/service/BetfairApiClient.java
+++ betfair/src/main/java/com/smt/betfair/service/BetfairApiClient.java
@@ -3,8 +3,6 @@ package com.smt.betfair.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
