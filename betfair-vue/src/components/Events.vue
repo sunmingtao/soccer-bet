53e031f8c0bf8eb57fe8a4236e0e3da4bacc4d30
Mingtao Sun <mingtao@Mingtaos-MacBook-Air.local>
2021-02-26 06:54:55 +1100

    display events in a table

--- betfair-vue/src/components/Events.vue
+++ betfair-vue/src/components/Events.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="">
-    <ul>
-      <li v-for="event in events" :key="event.id">
-        {{ event }}
-      </li>
-    </ul>
+    <v-client-table :data="events" :columns="columns" :options="options"> </v-client-table>
   </div>
 </template>
 
@@ -13,11 +9,22 @@
 export default {
   data () {
     return {
-      events: {}
+      events: [],
+      columns: ['id', 'name', 'countryCode', 'openDate'],
+      options: {
+        headings: {
+          id: 'Event ID',
+          name: 'Event Name',
+          countryCode: 'Country Code',
+          openDate: 'Match date'
+        },
+        sortable: ['countryCode', 'openDate'],
+        perPage: 1000,
+        perPageValues: [1000, 2000]
+      }
     }
   },
   beforeMount() {
-
     this.$http.get("events/1").then(response => {
       this.events = response.data.result.map(r => r.event);
     });
--- betfair-vue/src/main.js
+++ betfair-vue/src/main.js
@@ -4,9 +4,14 @@ import Events from './components/Events.vue'
 import EventOdds from './components/EventOdds.vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router';
+import {ClientTable} from 'vue-tables-2'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+Vue.use(ClientTable);
 
 const routes = [
   { path: '/', component: EventOdds },
